cc653602eccb33b4144f4855035d63d974474ef1
Akhil Karumanchi <76400408+akhilkarumanchi@users.noreply.github.com>
2024-04-28 22:36:38 +0530

    Spring Initilizer code

---
+++ src/main/java/com/example/lldcarrentalsystem/LldCarRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.lldcarrentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldCarRentalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldCarRentalSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lldcarrentalsystem/LldCarRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lldcarrentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldCarRentalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
